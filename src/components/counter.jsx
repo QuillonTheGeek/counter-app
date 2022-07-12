2ccce9860f6bca9193cc622e12dc89fae4723f6a
Quillon <quillonthegeek@gmail.com>
2022-07-12 07:23:00 -0700

    functioning delete and increment

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -77,10 +77,11 @@
 // HANDLING AND BINDING EVENTS
 
 import React, { Component } from "react";
+import Counters from "./counters";
 
 export class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.counter.value,
   };
   styles = {
     fontSize: 30,
@@ -92,24 +93,29 @@ export class Counter extends Component {
   //   }
 
   handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log(this.props);
+
     return (
       <div>
+        <h4> {this.props.id}</h4>
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={this.doHandleIncrement}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
-          increment
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -117,13 +123,13 @@ export class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "zero" : value;
   }
 }
 
